e64fb04e375b628b729ddb98ad1f1c34658d59dd
ojela028xamk <jerelans91@gmail.com>
2024-02-02 14:11:27 +0200

    add instruction form

--- src/Instructions/Instructions.tsx
+++ src/Instructions/Instructions.tsx
@@ -1,10 +1,29 @@
-import { JSX } from 'react'
+import { JSX, useState } from 'react'
 import css from './Instructions.module.scss'
+import { Button, Form, ListGroup } from 'react-bootstrap'
 
 const Instructions = (): JSX.Element => {
+  const [instructionItem, setInstructionItem] = useState<string>('')
+
   return (
     <div className={css.instructions}>
-      <h3>Instructions</h3>
+      <div className={css.instructions_add}>
+        <h3>Instructions</h3>
+        <Form>
+          <Form.Label>Instruction</Form.Label>
+          <Form.Control
+            type="text"
+            value={instructionItem}
+            placeholder="Add an instruction..."
+            onChange={(event) => setInstructionItem(event.currentTarget.value)}
+          ></Form.Control>
+        </Form>
+        <br />
+        <Button variant="success">Add an instruction +</Button>
+      </div>
+      <div className={css.instructions_list}>
+        <ListGroup></ListGroup>
+      </div>
     </div>
   )
 }
