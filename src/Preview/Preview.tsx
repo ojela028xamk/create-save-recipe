1a6a2282229ddd61e53b1a9c262a0f2cd7ad5cdc
ojela028xamk <jerelans91@gmail.com>
2024-02-05 16:33:28 +0200

    react-pdf library

--- src/Preview/Preview.tsx
+++ src/Preview/Preview.tsx
@@ -1,10 +1,18 @@
 import { JSX } from 'react'
 import css from './Preview.module.scss'
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer'
+import PreviewPDF from './PreviewPDF'
 
 const Preview = (): JSX.Element => {
   return (
     <div className={css.preview}>
       <h3>Preview</h3>
+      <PDFViewer>
+        <PreviewPDF />
+      </PDFViewer>
+      <PDFDownloadLink document={<PreviewPDF />} fileName="testi.pdf">
+        {({ loading }) => (loading ? 'Loading document...' : 'Download now!')}
+      </PDFDownloadLink>
     </div>
   )
 }
