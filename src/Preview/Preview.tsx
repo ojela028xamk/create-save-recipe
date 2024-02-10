9514205e5e9b32f884d4353a0095027096f24745
ojela028xamk <jerelans91@gmail.com>
2024-02-10 13:35:40 +0200

    new pdf library

--- src/Preview/Preview.tsx
+++ src/Preview/Preview.tsx
@@ -1,7 +1,5 @@
-// https://react-pdf.org/
 import { JSX, useState } from 'react'
 import css from './Preview.module.scss'
-import { PDFViewer } from '@react-pdf/renderer'
 import PreviewPDF from './PreviewPDF'
 import { IngredientItem, InstructionItem, StorageType } from '../globalTypes'
 import {
@@ -31,18 +29,12 @@ const Preview = (): JSX.Element => {
   return (
     <div className={css.preview}>
       <h3>Preview</h3>
+      <PreviewPDF
+        pdfRecipeName={pdfRecipeName}
+        pdfIngredients={pdfIngredients}
+        pdfInstructions={pdfInstructions}
+      />
       <button onClick={getStorage}>Render</button>
-      <PDFViewer>
-        <PreviewPDF
-          pdfRecipeName={pdfRecipeName}
-          pdfIngredients={pdfIngredients}
-          pdfInstructions={pdfInstructions}
-        />
-      </PDFViewer>
-      <br />
-      {/* <PDFDownloadLink document={<PreviewPDF />} fileName="testi.pdf">
-        {({ loading }) => (loading ? 'Loading document...' : 'Download PDF')}
-      </PDFDownloadLink> */}
     </div>
   )
 }
--- src/Preview/PreviewPDF.tsx
+++ src/Preview/PreviewPDF.tsx
@@ -1,7 +1,9 @@
-// https://react-pdf.org/
-import { Page, Text, View, Document, Image } from '@react-pdf/renderer'
 import { IngredientItem, InstructionItem } from '../globalTypes'
-import { styles } from './styles'
+import { useRef } from 'react'
+import html2canvas from 'html2canvas'
+import jsPDF from 'jspdf'
+import { Image } from 'react-bootstrap'
+import css from './Preview.module.scss'
 
 type PreviewPDFProps = {
   pdfRecipeName: string
@@ -14,35 +16,70 @@ const PreviewPDF = ({
   pdfIngredients,
   pdfInstructions,
 }: PreviewPDFProps): JSX.Element => {
+  const pdfRef = useRef(null)
+
+  const downloadPDF = (): void => {
+    const input = pdfRef.current
+
+    if (input) {
+      html2canvas(input).then((canvas) => {
+        const imgData = canvas.toDataURL('image/png')
+        const pdf = new jsPDF('p', 'mm', 'a4', true)
+        const pdfWidth = pdf.internal.pageSize.getWidth()
+        const pdfHeight = pdf.internal.pageSize.getHeight()
+        const imgWidth = canvas.width
+        const imgHeight = canvas.height
+        const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight)
+        const imgX = (pdfWidth - imgWidth * ratio) / 2
+        const imgY = 30
+        pdf.addImage(
+          imgData,
+          'PNG',
+          imgX,
+          imgY,
+          imgWidth * ratio,
+          imgHeight * ratio
+        )
+        pdf.save('testi123.pdf')
+      })
+    }
+  }
+
   return (
-    <Document>
-      <Page size="A4">
-        <View style={styles.header}>
-          <Text style={styles.header_text}>{pdfRecipeName}</Text>
-          <View style={styles.header_image}>
-            <Image src={'https://react-pdf.org/images/og-banner.png'}></Image>
-          </View>
-        </View>
-        <View style={styles.content}>
-          <View style={styles.ingredients}>
-            <Text>INGREDIENTS</Text>
-            {pdfIngredients.map((item, index) => (
-              <Text key={item.id}>
-                {index + 1}. {item.name} {item.amount} {item.unit}
-              </Text>
-            ))}
-          </View>
-          <View style={styles.instructions}>
-            <Text>INSTRUCTIONS</Text>
-            {pdfInstructions.map((item, index) => (
-              <Text key={item.id}>
-                {index + 1}. {item.step}
-              </Text>
-            ))}
-          </View>
-        </View>
-      </Page>
-    </Document>
+    <>
+      <div className={css.preview_pdf} ref={pdfRef}>
+        <div className={css.header}>
+          <h2 className={css.header_text}>{pdfRecipeName}</h2>
+          <Image
+            className={css.header_image}
+            src={'https://react-pdf.org/images/og-banner.png'}
+          />
+        </div>
+        <div className={css.content}>
+          <div className={css.ingredients}>
+            <h4>Ingredients</h4>
+            <ul>
+              {pdfIngredients.map((item, index) => (
+                <li key={item.id}>
+                  {index + 1}. {item.name} {item.amount} {item.unit}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={css.instructions}>
+            <h4>Instructions</h4>
+            <ul>
+              {pdfInstructions.map((item, index) => (
+                <li key={item.id}>
+                  {index + 1}. {item.step}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <button onClick={downloadPDF}>Save pdf!</button>
+    </>
   )
 }
 
